09713b59187d9baf9e21c36e06e5c5c1fa06248f
Nathan G. Marley <nathan.marley@gmail.com>
2013-04-08 20:37:34 -0500

    Removing microposts from tests.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -55,15 +55,15 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+    let!(:l1) { FactoryGirl.create(:list, user: user, name: "Foo") }
+    let!(:l2) { FactoryGirl.create(:list, user: user, name: "Bar") }
     before { visit user_path(user) }
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
-    describe "microposts" do
-      it { should have_content(m1.content) }
-      it { should have_content(m2.content) }
-      it { should have_content(user.microposts.count) }
+    describe "lists" do
+      it { should have_content(l1.name) }
+      it { should have_content(l2.name) }
+      it { should have_content(user.lists.count) }
     end
   end
 
