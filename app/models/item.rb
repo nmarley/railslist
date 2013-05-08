805b4943a656ef9bab4704a555a0c1078f72e76e
Nathan Marley <nathan.marley@gmail.com>
2013-05-08 15:42:51 -0500

    Cleaned up Item model implementation.
    Updating list_ts in the Item model using belongs_to with 'touch' option.
    Replaces List.update_all, which seems a bit hack-ish.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ActiveRecord::Base
   attr_accessible :content
-  belongs_to :list
+  belongs_to :list, touch: true
 
-  before_save { List.update_all("updated_at = '#{0.seconds.ago}'", "id = '#{list_id}'") }
   validates :list_id, presence: true
   validates :content, presence: true
 
