61bd1fd0f8ef9580127e314f0dd3b6bbdbb7f1f1
Nathan Marley <nathan.marley@gmail.com>
2013-08-15 18:20:22 -0700

    Update deploy.rb to use different user, commands.
    Also use different user, paths in unicorn.conf.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :scm, :git
 set :repository,  "gitolite@blackcarrot.be:railslist"
 set :scm_passphrase, ""
 
-set :user, "nmarley"
+set :user, "deploy"
 set :use_sudo, false
 
 #set :stages, %w[staging production]
@@ -25,16 +25,16 @@ set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && #{try_sudo} bundle exec unicorn -c #{unicorn_config} -E #{rails_env} -D"
+    run "cd #{current_path} && #{use_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill -s TERM `cat #{unicorn_pid}`"
+    run "#{use_sudo} kill -s TERM `cat #{unicorn_pid}`"
   end
   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
+    run "#{use_sudo} kill -s QUIT `cat #{unicorn_pid}`"
   end
   task :reload, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
+    run "#{use_sudo} kill -s USR2 `cat #{unicorn_pid}`"
   end
   task :restart, :roles => :app, :except => { :no_release => true } do
     if File.exists? unicorn_pid
--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -1,20 +1,10 @@
-# Minimal sample configuration file for Unicorn (not Rack) when used
-# with daemonization (unicorn -D) started in your working directory.
-#
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
-# See also http://unicorn.bogomips.org/examples/unicorn.conf.rb for
-# a more verbose configuration using more features.
-
 # start server with:
-# bundle exec unicorn -c unicorn.conf.rb -E production -D
+# unicorn -c unicorn.conf.rb -E production -D
 
-#listen 7007 # by default Unicorn listens on port 8080
 listen "/tmp/unicorn.sock"
 worker_processes 8 # this should be >= nr_cpus
-approot = "/var/www/railslist/current"
-pid "#{approot}/tmp/pids/unicorn.pid"
-stderr_path "#{approot}/log/unicorn.log"
-stdout_path "#{approot}/log/unicorn.log"
-user "uniwork", "uniwork"
-#working_directory = approot 
+approot = "/var/www/railslist"
+pid "#{approot}/shared/pids/unicorn.pid"
+stderr_path "#{approot}/shared/log/unicorn.log"
+stdout_path "#{approot}/shared/log/unicorn.log"
+user "deploy", "deploy"
