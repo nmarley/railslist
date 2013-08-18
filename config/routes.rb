46be5cb9cb8e5550bf7c9347e826db7c06748369
Nathan Marley <nathan.marley@gmail.com>
2013-08-18 09:08:39 -0700

    Add IP echo controller (show my client's ip).

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  get   '/whatismyip',  to: 'ip_echo#echo'
 end
