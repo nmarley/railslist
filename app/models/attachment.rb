7574991ef9cc76e3d6e1c2d8e8e6a7c015a7350c
Nathan Marley <nathan.marley@gmail.com>
2013-10-22 19:01:33 -0700

    Fix bug in viewing files

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ActiveRecord::Base
   belongs_to :list
-  has_attached_file :media
-    # :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-    # :url => "/system/:attachment/:id/:filename"
+  has_attached_file :media,
+    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    :url => "/system/:attachment/:style/:id/:filename"
 end
