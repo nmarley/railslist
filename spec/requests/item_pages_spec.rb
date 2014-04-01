3c88899e7e3ed3329f8e34d34199c484ce16f319
Nathan Marley <nathan.marley@gmail.com>
2014-03-31 17:18:43 -0700

    add some item tests

--- spec/requests/item_pages_spec.rb
+++ spec/requests/item_pages_spec.rb
@@ -43,16 +43,38 @@ describe "Item pages" do
         assert page.html.match('my favorite site is: <a href="http://www.google.com/" target="_blank">http://www.google.com/</a> ... yeah!')
       end
     end
+  end
 
-    # describe "clicking bump button" do
-    #   before do
-    #     ...
-    #   end
-    #   click_button "Bump"
-    #   it "should ..." do
-    #     assert something
-    #   end
-    # end
+  describe "item list" do
+    before do
+      list_name = "Seed List"
+      create_list list_name
+      click_link list_name
+
+      Timecop.freeze(1.hour.ago) do
+        create_item("Paul Robeson tomato seeds")
+        create_item("Black Krim Tomato Seeds")
+        create_item("Brandywine Tomato Seeds")
+      end
+    end
+
+    describe "clicking bump button" do
+      pending 'bump button'
+      # before do
+      #   ...
+      # end
+      # click_button "Bump"
+      # it "should move entry to the top of the item list" do
+      #   assert something
+      # end
+    end
+
+    describe "the item list" do
+      it "should match" do
+        # pp page.html
+        assert page.html.match('Paul Robeson')
+      end
+    end
   end
 
 end
