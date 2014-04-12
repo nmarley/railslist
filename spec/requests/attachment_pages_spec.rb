d48619c0c748d93cd1a05e3c1331ba3d00591127
Nathan Marley <nathan.marley@gmail.com>
2014-04-12 08:32:08 -0700

    finish attachment request spec

--- spec/requests/attachment_pages_spec.rb
+++ spec/requests/attachment_pages_spec.rb
@@ -17,9 +17,20 @@ describe "Attachment pages" do
 
     # obviously a placeholder for the actual test
     it "should update list timestamp" do
+      # grab the list id and List instance
+      list_id = find('#item_list_id').value.to_i
+      @list = List.find(list_id)
+
+      # make a temp file for uploading
+      tempfile = Tempfile.new('foo.txt')
+      tempfile.write("Hello, World!\n")
+      tempfile.close
+
       # add/upload attachment
+      attach_file('attachment[media]', tempfile.path)
+
       # verify timestamp for list has been updated
-      assert true
+      expect { click_button 'Add Attachment';@list.reload }.to change(@list, :updated_at)
     end
 
   end
