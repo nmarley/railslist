7d5db54bec1383fb5353e947c395231e97af4109
Nathan Marley <nathan.marley@gmail.com>
2014-05-14 16:30:57 -0700

    disable old rspec 'should' syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,4 +38,9 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # disable 'should' syntax
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
