b823c2bb94babbd461977b7749e0d666baa0ce36
Nathan Marley <nathan.marley@gmail.com>
2015-02-05 18:26:13 -0600

    fix deprecated config elements

--- config/application.rb
+++ config/application.rb
@@ -51,5 +51,7 @@ module RailsList
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
