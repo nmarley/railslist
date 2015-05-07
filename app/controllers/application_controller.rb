3dfc6cd62f0238990ec6687724d8da073ea580a3
Nathan Marley <nathan.marley@gmail.com>
2015-05-07 15:09:59 -0500

    protect with exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
   include SessionsHelper
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
